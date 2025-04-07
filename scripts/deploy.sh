ea88e303dbbc5ee8449260dd9a160e3dbcec33b9
Derrick Kearney <d_kearney@bellaliant.net>
2025-04-06 22:49:27 -0300

    Fix path to script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /root/digital-ocean-github-actions-ci
+cd /derrick/digital-ocean-github-actions-ci/scripts/deploy.sh
 git pull origin master
 /root/.bun/bin/bun install
 sudo systemctl restart digital-ocean-github-actions-ci
